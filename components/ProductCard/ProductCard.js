290afe95ee7904248c1611214298def2106bf585
nnabighods <n.nabighods@gmail.com>
2022-04-15 23:51:18 -0600

    display rtd

--- components/ProductCard/ProductCard.js
+++ components/ProductCard/ProductCard.js
@@ -3,25 +3,25 @@ import Image from 'next/image'
 import {productCard, price, name, description} from './styles.module.scss'
 
 function ProductCard ({children, product, ...props})  {
-  const {productName, productPrice, productDescription, imageUrl}= {...product}
+  const {productName, productPrice, productDescription, imageUrl, uid}= {...product}
   return (
        <aside className={productCard}>
          <header>
-            <Image
+           <Image
               src={imageUrl}
               alt={productName}
-              width={418}
-              height={240}
+              width={400}
+              height={500}
               quality={50}
-            />
-          
+           />
+        
          </header>
-          <p className={name}>{productName}</p>
+          <h2 className={name}>{productName}</h2>
           <p className={price}>${productPrice}</p>
           <p className={description}>{productDescription}</p>
           <footer>
             <form action="#" method="POST">
-              <input type="hidden" name="uid" value="uid"/>
+              <input type="hidden" name="uid" value={uid}/>
               <button type="submit">Buy Now</button>
             </form>
           </footer>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
+import Head from "next/head";
 import PageTitle from "../components/PageTitle/PageTitle";
+import ProductCard from "../components/ProductCard/ProductCard";
 
-export default function Home() {
+export default function Home(props) {
+  const products = props.products;
+
   return (
-    <PageTitle title="Seeder" tagline="For all your seed needs!"/> 
+    <>
+    <Head>
+      <meta charset="UTF-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Seeder</title>
+    </Head>
+    <PageTitle title="Seeder" tagline="For all your seed needs!"/>
+    <main>
+      {  products.map(product=> <ProductCard  key={product.uid} product={product}/>)}
+    </main>
+    </>
   )
+}
+
+export async function getStaticProps(){
+  const res = await fetch('https://storefront-e9f7d-default-rtdb.firebaseio.com/products.json')
+  const productData = await res.json();
+  const products = Object.values(productData)
+  return {
+      props:{
+          products
+      }
+  }
 }
